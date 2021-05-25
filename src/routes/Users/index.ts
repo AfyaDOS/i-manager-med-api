7cf87408608b54d9a042fb52d09b572862f01ff4
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-05-25 18:32:02 -0300

    delete user

---
+++ src/routes/Users/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from 'express';
+import UserController from '../../controllers/UserController';
+import authMiddleware from '../../middlewares/authMiddleware';
+
+
+const routesUsers = Router();
+
+routesUsers.get('/', authMiddleware, UserController.index);
+routesUsers.post('/', UserController.createUser);
+routesUsers.delete('/:id', UserController.deleteUser);
+
+
+
+export { routesUsers };
