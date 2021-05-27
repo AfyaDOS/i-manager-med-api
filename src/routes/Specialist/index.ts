e1952834441e00d47df8587008a668179d0c0bd0
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-05-27 13:45:39 -0300

    Specilist create e select por id de User

--- src/routes/Specialist/index.ts
+++ src/routes/Specialist/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import SpecialistController from '../../controllers/SpecialistController';
+import authMiddleware from '../../middlewares/authMiddleware';
 
 const routesSpecialist = Router();
 
-routesSpecialist.get('/', SpecialistController.index);
-routesSpecialist.post('/', SpecialistController.createSpecialist);
+routesSpecialist.get('/', authMiddleware, SpecialistController.index);
+routesSpecialist.post(
+  '/',
+  authMiddleware,
+  SpecialistController.createSpecialist,
+);
 
 export { routesSpecialist };
