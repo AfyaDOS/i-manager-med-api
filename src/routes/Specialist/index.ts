fb0ea25aaee5fcb352dbc84e2e504f6145c4f298
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-05-27 23:20:40 -0300

    crud Specialist e Spelialties

--- src/routes/Specialist/index.ts
+++ src/routes/Specialist/index.ts
@@ -5,10 +5,22 @@ import authMiddleware from '../../middlewares/authMiddleware';
 const routesSpecialist = Router();
 
 routesSpecialist.get('/', authMiddleware, SpecialistController.index);
+
 routesSpecialist.post(
   '/',
   authMiddleware,
   SpecialistController.createSpecialist,
+);
+
+routesSpecialist.put(
+  '/:id',
+  authMiddleware,
+  SpecialistController.updateSpecialist,
+);
+routesSpecialist.delete(
+  '/:id',
+  authMiddleware,
+  SpecialistController.deleteSpecialist,
 );
 
 export { routesSpecialist };
