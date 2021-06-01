dccb6459ae74cc11d82cae2280f3330a82f34515
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-06-01 18:53:32 -0300

    merge db

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,33 +1,32 @@
-import { getConnectionOptions, createConnection, getConnection } from "typeorm";
+import { getConnectionOptions, createConnection, getConnection } from 'typeorm';
 
 const createTypeormConn = {
-    async create(){
-        const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
+  async create() {
+    const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
-        return createConnection({ ...connectionOptions, name: "default" });
-    },
-  
-    async close(){
-      await getConnection().close(); 
-    },
-  
-    async clear(){
-     try {
-        const connection = getConnection('default');
-        const entities = connection.entityMetadatas;
-      
-        const promises = entities.map(async (entity) => {
-          const repository = connection.getRepository(entity.name);
-          return repository.query(`DELETE FROM ${entity.tableName}`);
-        });
-  
-        return Promise.all(promises)
-     } catch (error) {
-         throw new Error("Erro ao limpar as tabelas");
-     }
-    },
-  };
-  export {createTypeormConn};
+    return createConnection({ ...connectionOptions, name: 'default' });
+  },
 
+  async close() {
+    await getConnection().close();
+  },
 
-createTypeormConn.create()
+  async clear() {
+    try {
+      const connection = getConnection('default');
+      const entities = connection.entityMetadatas;
+
+      const promises = entities.map(async (entity) => {
+        const repository = connection.getRepository(entity.name);
+        return repository.query(`DELETE FROM ${entity.tableName}`);
+      });
+
+      return Promise.all(promises);
+    } catch (error) {
+      throw new Error('Erro ao limpar as tabelas');
+    }
+  },
+};
+export { createTypeormConn };
+
+createTypeormConn.create();
--- src/routes/Specialist/index.ts
+++ src/routes/Specialist/index.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import SpecialistController from '../../controllers/SpecialistController';
-import authMiddleware from '../../middlewares/authMiddleware';
 
 const routesSpecialist = Router();
 
