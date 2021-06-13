6a853799633101aed9fcef0c64e14a0888599151
Marcelo Franchini <marcelo_franchini@hotmail.com>
2021-06-13 15:24:18 -0300

    Swagger Atualização - Especialistas e Usuários

--- src/controllers/SpecialtiesController/index.ts
+++ src/controllers/SpecialtiesController/index.ts
@@ -14,6 +14,7 @@ class SpecialtiesController {
       await connection.close();
       return res.status(200).json(SpecialtiesExists);
     } catch (error) {
+      await connection.close();
       return res.status(404).json({ error: true, message: error.message });
     }
   }
@@ -40,6 +41,7 @@ class SpecialtiesController {
 
       return res.status(200).json(specialties);
     } catch (error) {
+      await connection.close();
       return res.status(404).json({ error: true, message: error.message });
     }
   }
